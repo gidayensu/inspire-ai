f4c7ad33feea004ac59fbb3fb114b32e4aede2bf
giadyensukwabi <thurgid@gmail.com>
2024-03-26 19:11:53 +0000

    changed play page

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,33 +1,8 @@
-import AudioItem from "@/components/UIs/AudioItem";
-import TopNav from "@/components/nav/TopNav";
-import BottomNavBar from "@/components/nav/BottomNavBar";
-
-const  Player = ({ params }: { params: { category: string } }) => {
+import PlayerUi from "@/components/UIs/PlayerUi";
+const  Player = () => {
   return (
-    <>
-    <div className="mb-3 mx-5">
-     <TopNav />
-     </div>
-  <main className="mx-5 flex flex-col gap-2">
-     
-     <div className="flex flex-col justify-center items-center">
-        <h2 className="font-bold text-black text-4xl">
-          HOPE
-        </h2>
-        <p className="opacity-50 text-black  text-[12px] text-center">
-          <span className="font-bold ">Did you know?</span> listening to words of encouragement can help improve your mood!
-        </p>
-     </div>
-    <AudioItem title="hope" desc="hope" image="hope"/>
-    <AudioItem title="hope" desc="hope" image="hope"/>
-    <AudioItem title="hope" desc="hope" image="hope"/>
-    <AudioItem title="hope" desc="hope" image="hope"/>
+    <PlayerUi params={{inspire: ''}}/>
     
-    </main>
-    <div>
-      <BottomNavBar/>
-    </div>
-    </>
     )
 }
 
